126cc0db57f32f54dab448bcd12fee5f44b80ba8
Sebastian Meier <contact@sebastianmeier.eu>
2023-01-16 21:44:49 +0100

    fix cors problem

--- iterative-ui/src/stores/taxonomies.ts
+++ iterative-ui/src/stores/taxonomies.ts
@@ -8,7 +8,7 @@ let loaded = false;
 export const load = (): void => {
   if (!loaded) {
     loaded = true;
-    fetch("https://github.com/sebastian-meier/chi23-human-ai-demo/raw/main/iterative-ui/public/data/demo-taxonomies.json")
+    fetch("https://raw.githubusercontent.com/sebastian-meier/chi23-human-ai-demo/raw/main/iterative-ui/public/data/demo-taxonomies.json")
       .then((response) => response.json())
       .then((response) => {
         taxonomies.set(response);
